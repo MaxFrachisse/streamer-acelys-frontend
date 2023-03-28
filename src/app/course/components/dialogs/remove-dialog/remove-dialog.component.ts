8c49bb3d2fa0c6db82e3f04fe4f1cafd210ece1b
Max <frachisse.max@gmail.com>
2023-03-28 09:49:29 +0200

    forgot to subscribe to delete observable ... (US 5 done)

--- src/app/course/components/dialogs/remove-dialog/remove-dialog.component.ts
+++ src/app/course/components/dialogs/remove-dialog/remove-dialog.component.ts
@@ -1,3 +1,4 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CourseService } from 'src/app/course/services/course.service';
@@ -13,12 +14,16 @@ export class RemoveDialogComponent implements OnInit {
     public dialogRef: MatDialogRef<RemoveDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     public courseService: CourseService
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     console.log(this.data)
   }
-  onSubmitDelete(){
-    this.courseService.remove(this.data.id)
+  onSubmitDelete() {
+    this.courseService.remove(this.data.id).subscribe({
+      next: ((Response: HttpResponse<any>) => {
+        console.log(`Voici l'id du cours supprimer : ${this.data.id}`)
+      })
+    })
   }
 }
