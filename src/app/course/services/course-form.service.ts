62d3dc3063f6e5d28c684de0159a451f6253a510
Max <frachisse.max@gmail.com>
2023-03-29 15:31:53 +0200

    continuing course form service ?

--- src/app/course/services/course-form.service.ts
+++ src/app/course/services/course-form.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CourseModel } from '../models/course-model';
 import { CourseService } from './course.service';
 
 @Injectable({
@@ -7,11 +8,21 @@ import { CourseService } from './course.service';
 })
 export class CourseFormService {
   public _form: FormGroup = new FormGroup({})
+  public course: CourseModel = new CourseModel()
 
   constructor(
     private _formBuilder: FormBuilder,
     private _courseService: CourseService
   ) { }
+
+  public buildForm(course: CourseModel): void {
+    this.course = course
+    this._buildForm()
+  }
+
+  get form(): FormGroup {
+    return this._form
+  }
 
   public get c(): {[key: string]: AbstractControl} {
     return this._form.controls
@@ -20,7 +31,7 @@ export class CourseFormService {
   private _buildForm(): void {
     this._form = this._formBuilder.group({
       title: [
-        '',
+        this.course.title,
         [
           Validators.required
         ]
