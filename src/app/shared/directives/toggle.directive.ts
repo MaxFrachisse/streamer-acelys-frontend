ced8aa61e7866f49a546aa6f7e9e28516da54e88
MaxFrachisse <frachisse.max@gmail.com>
2023-04-04 09:06:42 +0200

    change in directive

--- src/app/shared/directives/toggle.directive.ts
+++ src/app/shared/directives/toggle.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, EventEmitter, HostListener, Input, OnInit, Output, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, Input, OnInit, Output, Renderer2, ViewContainerRef } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Directive({
   selector: 'toggle'
@@ -7,34 +8,51 @@ export class ToggleDirective implements OnInit {
   private _nativeElement;
   private _span: any
   private _icon: string = '>'
+  private _nativeIcon: any
+  private _nativeIconContent: string = 'chevron_right'
   private _iconStatus: boolean = true
 
-  @Input() public selectedStatus: boolean = false
+  @Input() public selectedStatus : boolean = false
+
   @Input() public set isExpandable(status: boolean) {
     this._iconStatus = status
     if (!status) {
       this._icon = '-'
+      this._nativeIconContent = 'remove'
     } else {
       this._icon = '>'
+      this._nativeIconContent = 'chevron_right'
     }
   }
+
+  @Input() public useIcon: boolean = false
+
   @Output() public onToggle: EventEmitter<boolean> = new EventEmitter()
+
   constructor(
     private _elementRef: ElementRef,
-    private _renderer: Renderer2
+    private _renderer: Renderer2,
+    private _viewContainerRef: ViewContainerRef,
   ) {
     this._nativeElement = _elementRef.nativeElement
   }
 
   ngOnInit(): void {
     this._span = this._renderer.createElement('span')
-    this._span.textContent = this._icon
-    this._renderer.setAttribute(this._span, 'title', `${this.selectedStatus ? 'Hide' : 'Show'}}`)
 
-    // Add some classes to span
-    this._renderer.addClass(this._span, 'icon-list')
-    this._renderer.addClass(this._span, 'up')
-
+    if (this.useIcon) {
+      // @todo : Check if MatIcon is registered before to
+      const icon = this._viewContainerRef.createComponent(MatIcon)
+      this._nativeIcon = icon.instance._elementRef.nativeElement
+      this._nativeIcon.textContent = this._nativeIconContent
+      this._renderer.appendChild(this._span, this._nativeIcon)
+    } else {
+      this._span.textContent = this._icon
+      // Add some classes to span
+      this._renderer.addClass(this._span, 'icon-list')
+      this._renderer.addClass(this._span, 'up')
+      this._renderer.setAttribute(this._span, 'title', `${this.selectedStatus ? 'Hide' : 'Show'}}`)
+    }
     this._renderer.appendChild(this._nativeElement,
       this._span
     )
@@ -44,14 +62,23 @@ export class ToggleDirective implements OnInit {
   onClick(): void {
     if (this._iconStatus) {
       this.selectedStatus = !this.selectedStatus
-      if (this.selectedStatus) {
-        this._renderer.removeClass(this._span, 'up')
-        this._renderer.addClass(this._span, 'down')
-      } else {
-        this._renderer.removeClass(this._span, 'down')
-        this._renderer.addClass(this._span, 'up')
-      }
       this.onToggle.emit(this.selectedStatus)
+      if (!this.useIcon) {
+        if (this.selectedStatus) {
+          this._renderer.removeClass(this._span, 'up')
+          this._renderer.addClass(this._span, 'down')
+        } else {
+          this._renderer.removeClass(this._span, 'down')
+          this._renderer.addClass(this._span, 'up')
+        }
+      } else {
+        if (this.selectedStatus) {
+          this._nativeIcon.textContent = 'expand_more'
+        } else {
+          this._nativeIcon.textContent = 'chevron_right'
+        }
+
+      }
     }
   }
 }
